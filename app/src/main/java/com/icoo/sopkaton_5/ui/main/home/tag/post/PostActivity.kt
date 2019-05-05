72aab1a6ed3d897ed5d9873e9a9388d1b88f8566
jihye0420 <wlgp85699845@gmail.com>
2019-05-06 06:44:21 +0900

    postadd

--- app/src/main/java/com/icoo/sopkaton_5/ui/main/home/tag/post/PostActivity.kt
+++ app/src/main/java/com/icoo/sopkaton_5/ui/main/home/tag/post/PostActivity.kt
@@ -2,6 +2,7 @@ package com.icoo.sopkaton_5.ui.main.home.tag.post
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.icoo.sopkaton_5.R
 
 class PostActivity : AppCompatActivity() {
 

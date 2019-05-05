354fb62431cd047298b4ef4c4b5f835ee847aacb
jhm6258680 <jhm6258680@naver.com>
2019-05-05 17:12:11 +0900

    feat: Initial commit

---
+++ app/src/main/java/com/icoo/sopkaton_5/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.icoo.sopkaton_5.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.icoo.sopkaton_5.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
